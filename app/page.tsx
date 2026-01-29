95fe29755115fdaf575c8ff179c5c5b244dc1626
Daniel Yan <daniel.yan@iestgroup.com>
2026-01-29 13:21:10 -0300

    feat: Add responsive code

--- app/page.tsx
+++ app/page.tsx
@@ -22,12 +22,12 @@ export default function Home() {
   return (
     <div>
       <main className="bg-white">
-        <div className="flex justify-between items-center max-w-7xl mx-auto py-10">
-          <div className="w-1/2 p-2.5 flex flex-col gap-5">
-            <h2 className="font-bold uppercase leading text-primary text-[32px]">
+        <div className="flex flex-col lg:flex-row justify-between items-center max-w-7xl mx-auto py-6 md:py-10 px-4">
+          <div className="w-full lg:w-1/2 p-2.5 flex flex-col gap-5">
+            <h2 className="font-bold uppercase leading text-primary text-2xl md:text-[28px] lg:text-[32px]">
               Nossa história
             </h2>
-            <p className="text-[19px] font-medium">
+            <p className="text-base md:text-lg lg:text-[19px] font-medium">
               Desde 2012, fornecendo serviços e soluções empresariais completas
               para multinacionais que buscam ingressar e prosperar no mercado
               brasileiro.
@@ -36,19 +36,19 @@ export default function Home() {
               <Button>Saiba mais</Button>
             </Link>
           </div>
-          <div className="w-1/4">
+          <div className="w-full lg:w-1/4 mt-6 lg:mt-0">
             <Image src={MainImage} alt="globo" />
           </div>
         </div>
       </main>
       <section className="bg-primary">
-        <div className="flex justify-between items-center max-w-7xl mx-auto py-10">
-          <div className="w-1/2 p-2.5 flex flex-col gap-5">
-            <h2 className="font-bold uppercase leading text-white text-[32px]">
+        <div className="flex flex-col lg:flex-row justify-between items-center max-w-7xl mx-auto py-6 md:py-10 px-4">
+          <div className="w-full lg:w-1/2 p-2.5 flex flex-col gap-5">
+            <h2 className="font-bold uppercase leading text-white text-2xl md:text-[28px] lg:text-[32px]">
               O que fazemos
             </h2>
 
-            <ul className="text-[28px] text-white font-bold *:hover:text-light-primary *:transition-colors *:duration-200 *:leading-[1.7em]">
+            <ul className="text-xl md:text-2xl lg:text-[28px] text-white font-bold *:hover:text-light-primary *:transition-colors *:duration-200 *:leading-[1.7em]">
               <li>
                 <Link href="/consultoria-profissional" className="">
                   Consultoria Profissional
@@ -77,23 +77,23 @@ export default function Home() {
               <Button variant="inverted">Saiba Mais</Button>
             </Link>
           </div>
-          <div className="w-2/5 p-2.5">
+          <div className="w-full lg:w-2/5 p-2.5 mt-6 lg:mt-0">
             <Image src={Section1Image} alt="imagem seção 1" />
           </div>
         </div>
       </section>
       <section className="bg-white">
-        <div className="flex justify-between items-center max-w-7xl mx-auto py-10">
-          <div className="w-1/2 p-2.5 flex flex-col gap-5">
-            <h2 className="font-bold uppercase leading text-primary text-[32px]">
+        <div className="flex flex-col lg:flex-row justify-between items-center max-w-7xl mx-auto py-6 md:py-10 px-4">
+          <div className="w-full lg:w-1/2 p-2.5 flex flex-col gap-5">
+            <h2 className="font-bold uppercase leading text-primary text-2xl md:text-[28px] lg:text-[32px]">
               Nosso diferencial
             </h2>
-            <p className="text-[19px] font-medium">
+            <p className="text-base md:text-lg lg:text-[19px] font-medium">
               O <strong className="font-bold">IEST Group</strong> conta com uma
               equipe altamente experiente e qualificada para atender às demandas
               em serviços empresariais.
             </p>
-            <p className="text-[19px] font-medium">
+            <p className="text-base md:text-lg lg:text-[19px] font-medium">
               Nossos profissionais, além de possuírem profundo conhecimento
               técnico, são bilíngues e estão sempre atualizados com as
               tendências e necessidades do mercado, garantindo um atendimento de
@@ -103,25 +103,25 @@ export default function Home() {
               <Button>Saiba mais</Button>
             </Link>
           </div>
-          <div className="w-3/8">
+          <div className="w-full lg:w-3/8 mt-6 lg:mt-0">
             <Image src={Section2Image} alt="imagem seção 2" />
           </div>
         </div>
       </section>
-      <section>
-        <h2 className="font-bold uppercase leading text-primary text-[32px]">
+      <section className="px-4 py-6 md:py-10">
+        <h2 className="font-bold uppercase leading text-primary text-2xl md:text-[28px] lg:text-[32px]">
           Nossos Clientes
         </h2>
       </section>
       <section
-        className="relative bg-center bg-no-repeat bg-cover"
+        className="relative bg-center bg-no-repeat bg-cover px-4"
         style={style}
       >
-        <div className="w-3/5 py-10 p-2.5 flex flex-col gap-10">
-          <p className="font-medium leading-[1.3em] text-white text-[35px]">
+        <div className="w-full  max-w-7xl mx-auto md:w-4/5 lg:w-3/5 py-6 md:py-10 p-2.5 flex flex-col gap-6 md:gap-10">
+          <p className="font-medium leading-[1.3em] text-white text-2xl md:text-3xl lg:text-[35px]">
             Inicie seu negócio no Brasil com o IEST Group.
           </p>
-          <h1 className="font-medium leading-[1.3em] text-white text-[35px] ">
+          <h1 className="font-medium leading-[1.3em] text-white text-2xl md:text-3xl lg:text-[35px]">
             Expanda sua empresa para outros mercados com o IEST Group. Nós
             sabemos o caminho!
           </h1>
--- components/Contacts.tsx
+++ components/Contacts.tsx
@@ -6,36 +6,36 @@ import Button from "./Button";
 export default function Contacts() {
   return (
     <section className="bg-primary text-white">
-      <div className="flex justify-between items-center max-w-7xl mx-auto py-10">
-        <div className="w-1/4 p-2.5 flex flex-col gap-5">
+      <div className="flex flex-col lg:flex-row justify-between items-start max-w-7xl mx-auto py-6 md:py-10 px-4 gap-8 lg:gap-0">
+        <div className="w-full lg:w-1/4 p-2.5 flex flex-col gap-5">
           <div className="flex gap-5 items-center">
             <Image
               src={AddressIcon}
               alt="icone"
               className="h-8.75 text-white fill-white w-auto"
             />
-            <h2 className="font-bold uppercase leading text-[32px]">
+            <h2 className="font-bold uppercase leading text-2xl md:text-[28px] lg:text-[32px]">
               Endereço
             </h2>
           </div>
-          <p className="text-[19px] font-semibold">
+          <p className="text-base md:text-lg lg:text-[19px] font-semibold">
             Rua do Paraíso, 595 <br />
             11º andar - Paraíso <br />
             São Paulo/SP
           </p>
-          <p className="text-[18px]">
+          <p className="text-base md:text-lg lg:text-[18px]">
             Em caso de dúvida entre em contato através do e-mail{" "}
             <strong className="font-bold">br@iestgroup.com </strong> ou pelo
             telefone <strong className="font-bold">+55 11 2309-5904 </strong>
           </p>
         </div>
-        <div className="w-3/5 text-[18px]  p-2.5">
+        <div className="w-full lg:w-3/5 text-base md:text-lg lg:text-[18px] p-2.5">
           <div className="mb-5">
             Preencha seus dados abaixo e esclareça suas dúvidas:
           </div>
-          <form className="grid grid-cols-2 gap-4">
+          <form className="grid grid-cols-1 md:grid-cols-2 gap-4">
             <input
-              className="px-5 py-2 col-span-2 border border-white"
+              className="px-5 py-2 col-span-1 md:col-span-2 border border-white"
               placeholder="Nome"
             />
             <input
@@ -47,7 +47,7 @@ export default function Contacts() {
               placeholder="Telefone"
             />
             <textarea
-              className="px-5 py-2 col-span-2 border border-white"
+              className="px-5 py-2 col-span-1 md:col-span-2 border border-white"
               placeholder="Mensagem"
               rows={4}
             />
