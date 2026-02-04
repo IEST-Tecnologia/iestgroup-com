2cd48d9849f9bf63b2ae5471c7175e46d190c596
th3uuz <matheuslcd@hotmail.com>
2026-02-04 10:31:30 -0300

    feat: create a consent pop up

---
+++ app/actions/consent.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { revalidatePath } from "next/cache";
+
+export async function setConsentCookie(
+  value: "accepted" | "rejected",
+  pathname: string
+) {
+  const cookieStore = await cookies();
+
+  cookieStore.set("consent", value, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    maxAge: 60 * 60 * 24 * 365, // 1 year
+    path: "/",
+  });
+
+  revalidatePath(pathname);
+}
+
+export async function getConsentCookie() {
+  const cookieStore = await cookies();
+  return cookieStore.get("consent")?.value;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
+import ConsentPopUp from "@/components/ConsentPopUp";
+import { getConsentCookie } from "./actions/consent";
+import { Analytics } from "@vercel/analytics/next";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -9,27 +12,32 @@ const poppins = Poppins({
   weight: ["300", "400", "500", "600", "700"],
 });
 
-
 export const metadata: Metadata = {
   title: {
-    template: '%s - IEST Group',
-    default: 'IEST Group'
+    template: "%s - IEST Group",
+    default: "IEST Group",
   },
-  description: "Consultoria empresarial especializada em negócios entre China e Brasil. Apoiamos sua empresa em áreas administrativa, legal e contratação.",
+  description:
+    "Consultoria empresarial especializada em negócios entre China e Brasil. Apoiamos sua empresa em áreas administrativa, legal e contratação.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const consent = await getConsentCookie();
   return (
     <html lang="en">
-      <body
-        className={`${poppins.variable} antialiased`}
-      >
+      <body className={`${poppins.variable} antialiased`}>
         {children}
         <Footer />
+        {consent === "accepted" && (
+          <>
+            <Analytics />
+          </>
+        )}
+        <ConsentPopUp />
       </body>
     </html>
   );
